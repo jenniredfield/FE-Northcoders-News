ccbb753ecf313c1321144e2cc9c2ae239a76928c
Jen Bird <jenniredfield@hotmail.com>
2018-02-13 17:02:03 +0000

    Added picture to 404 Page

--- src/Notfound.js
+++ src/Notfound.js
@@ -13,6 +13,7 @@ class Notfound extends React.Component {
        
                 <div className="not-found">
                     <h1>Sorry, the page you are looking for does not exist</h1>
+                    <img src="https://www.elegantthemes.com/blog/wp-content/uploads/2017/07/404-error.png" alt='404' width="200" style={{marginTop: "50px" }}/>
                 </div>
             
 
